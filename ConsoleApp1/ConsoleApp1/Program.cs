4317a761c68a78cadc5f64e996899f1ae21208be
unknown <nandiniswaraj13@gmail.com>
2021-03-10 15:30:25 +0530

    [Nandini swaraj]Added Gambling Code to UC4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,9 @@ namespace ConsoleApp1
         public static int win = 0;
         public static int loss = 0;
         public static int flag = 0;
+        int[] cashWon = new int[20];
+        int[] cashLoss = new int[20];
+
 
         Random random = new Random();
 
@@ -60,12 +63,76 @@ namespace ConsoleApp1
 
         }
 
+        public void winOrLossFor20Day()
+        {
+            int lossStake = stake / 2;
+            int winStake = (stake * 3) / 2;
+            int totalWin = 0;
+            int totalLoss = 0;
+            int totalCash = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                int cash = stake;
+                win = 0;
+                loss = 0;
+                while (cash > 0)
+                {
+                    int check = random.Next(0, 2);
+                    if (check == 1)
+                    {
+                        
+                        win++;
+                        cash++;
+                    }
+                    else
+                    {
+                        
+                        loss++;
+                        cash--;
+                    }
+                    if (cash == winStake || cash == lossStake)
+                    {
+                        break;
+                    }
+
+                }
+                if (win > loss)
+                {
+                    cashWon[i] = (cash - stake);
+                }
+                else
+                {
+                    cashLoss[i] = (stake - cash);
+                }
+                totalCash = totalCash + cash - stake;
+                totalWin = totalWin + win;
+                totalLoss = totalLoss + loss;
+                cashWon[i] = cash;
+                cashLoss[i] = cash;
+               
+            }
+            Console.WriteLine("Total cash for day: " + totalCash);
+            Console.WriteLine();
+            for (int i=0;i< cashWon.Length;i++)
+            {
+                Console.WriteLine("Cash Won Per day: "+ cashWon[i]);
+            }
+            for (int i = 0; i < cashLoss.Length; i++)
+            {
+                Console.WriteLine("Cash Loss per day "+cashLoss[i]);
+            }
+            Console.WriteLine("Number of Winning times:  " + totalWin);
+            Console.WriteLine("Number of lossing times:  " + totalLoss);
+
+
+        }
+
 
         static void Main(string[] args)
         {
             Program obj = new Program();
-            int result = obj.winOrLossForADay();
-            Console.WriteLine("Playe stake for day " + stake);
-        }
+            obj.winOrLossFor20Day();
+                }
     }
 }
