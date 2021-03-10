5e162f3fcc983cd440673cb1b149fd9418c0a8cf
unknown <nandiniswaraj13@gmail.com>
2021-03-10 14:35:57 +0530

    [Nandini swaraj]Added Gambling Code to UC2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,10 +6,36 @@ namespace ConsoleApp1
     {
         public static int stake = 100;
         public static int bet = 1;
+        public static int win = 0;
+        public static int loss = 0;
+        public static int flag = 0;
+
+        Random random = new Random();
+
+        public void winOrLoss()
+        {
+
+            int check = random.Next(0, 2);
+            if (check == 1)
+            {
+                win++;
+                stake++;
+                Console.WriteLine("Player Win the game");
+            }
+            else
+            {
+                loss++;
+                stake--;
+                Console.WriteLine("Player loss the game");
+
+            }
+        }
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program obj = new Program();
+            obj.winOrLoss();
         }
     }
 }
