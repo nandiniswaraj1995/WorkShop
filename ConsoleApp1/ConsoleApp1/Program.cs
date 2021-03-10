458e46804c3ef2dbf872410ccdcab23d53fb9485
unknown <nandiniswaraj13@gmail.com>
2021-03-10 14:32:16 +0530

    [Nandini swaraj]Added Gambling Code to UC1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static int stake = 100;
+        public static int bet = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
